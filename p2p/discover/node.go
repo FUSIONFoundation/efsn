446f4608d710253074881a05a0b73436af1545b3
Bin0z <jiangbinsw@gmail.com>
2018-12-11 12:17:08 -0500

    bootnode support domain

--- p2p/discover/node.go
+++ p2p/discover/node.go
@@ -176,9 +176,12 @@ func parseComplete(rawurl string) (*Node, error) {
 	if err != nil {
 		return nil, fmt.Errorf("invalid host: %v", err)
 	}
-	if ip = net.ParseIP(host); ip == nil {
-		return nil, errors.New("invalid IP address")
+
+	ips, err := net.LookupIP(host)
+	if err != nil || len(ips) < 1 {
+		return nil, errors.New("invalid host")
 	}
+	ip = ips[0]
 	// Ensure the IP is 4 bytes long for IPv4 addresses.
 	if ipv4 := ip.To4(); ipv4 != nil {
 		ip = ipv4
