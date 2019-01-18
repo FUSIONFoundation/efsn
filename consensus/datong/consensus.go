61aa30bdc50e5355a5eb2fe2d85f7be90792b113
bret-fusion <bret@fusion.org>
2019-01-18 18:26:27 -0500

    bridge repair to test bad block fix

--- consensus/datong/consensus.go
+++ consensus/datong/consensus.go
@@ -119,7 +119,7 @@ func (dt *DaTong) VerifyHeader(chain consensus.ChainReader, header *types.Header
 	if header.Time.Cmp(big.NewInt(time.Now().Unix())) > 0 {
 		return consensus.ErrFutureBlock
 	}
-	return dt.VerifySeal(chain, header)
+	return nil // dt.VerifySeal(chain, header)
 }
 
 // VerifyHeaders is similar to VerifyHeader, but verifies a batch of headers
