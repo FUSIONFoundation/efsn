d4b9f425cfdc19d3fc91c09da30c4df15d39542e
Joey <JoeyKhd@users.noreply.github.com>
2019-04-07 13:35:22 +0200

    Update fsnNode.sh

--- QuickNodeSetup/fsnNode.sh
+++ QuickNodeSetup/fsnNode.sh
@@ -11,7 +11,7 @@ installNode(){
 	pause
 	echo "Updating packages"
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-	sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable
 	sudo apt-get update > dev/null
 	clear
 	echo "✓ Updated packages"
