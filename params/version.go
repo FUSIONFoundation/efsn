0b8a15faf4f86560caf2baee5c3a9b5defe91553
bret-fusion <bret@fusion.org>
2019-04-22 06:24:19 -0400

    incrementing version

--- params/version.go
+++ params/version.go
@@ -29,7 +29,7 @@ const (
 	// and put its version into the hundreds place
 	// so 16001 means ethereum VersionPath=16
 	// efsn patch = 001
-	VersionPatch = 16004 // Patch version component of the current release
+	VersionPatch = 16005 // Patch version component of the current release
 
 	VersionMeta = "stable" // Version metadata to append to the version string
 )
