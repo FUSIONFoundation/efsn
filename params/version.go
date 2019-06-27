8bf2defbfaa9c4e8cd8755b8dbf81e4efc2b7e4d
bret-fusion <bret@fusion.org>
2019-06-27 09:10:41 -0400

    update version for new test

--- params/version.go
+++ params/version.go
@@ -29,7 +29,7 @@ const (
 	// and put its version into the hundreds place
 	// so 16001 means ethereum VersionPath=16
 	// efsn patch = 001
-	VersionPatch = 16012 // Patch version component of the current release
+	VersionPatch = 16014 // Patch version component of the current release
 
 	VersionMeta = "unstable" // Version metadata to append to the version string
 )
