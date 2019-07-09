e398aa961763ab10bf114f78a62f2c95b4927af3
Iruwen <iruwen@gmx.net>
2019-07-09 15:42:48 +0200

    Added option --disable-mining and a few minor cosmetical changes
    To maximise node uptime it is desirable to have a fully synced secondary node in hot standby which does everything but mine. While the miner API module allows controlling the mining state while the node is running, it's preferable to have a safe way of launching the node with mining safely disabled independent of other settings like --autobt. To avoid confusion, breaking any existing configurations or accidentally disabling mining, the default is mining = true, also there's only a long option --disable-mining.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,31 +5,33 @@ DATA_DIR=$NODES_ROOT/data
 KEYSTORE_DIR=$DATA_DIR/keystore
 unlock=
 ethstats=
-autobt=false
+autobt="false"
+mining="true"
 
-display_usage() { 
-    echo "Commands for Fusion efsn:" 
-    echo -e "\n-e value    Reporting name of a ethstats service" 
-    echo -e "\n-u value    Account to unlock" 
-    echo -e "\n-a          Auto buy tickets" 
-    } 
+display_usage() {
+    echo "Commands for Fusion efsn:"
+    echo -e "-e value    Reporting name of a ethstats service"
+    echo -e "-u value    Account to unlock"
+    echo -e "-a          Auto buy tickets"
+    }
 
 while [ "$1" != "" ]; do
     case $1 in
         -u | --unlock )         shift
                                 unlock=$1
                                 ;;
-        -e | --ethstats )           shift
+        -e | --ethstats )       shift
                                 ethstats=$1
                                 ;;
-        -a | --autobt )         autobt=true
+        -a | --autobt )         autobt="true"
+                                ;;
+        --disable-mining )      mining="false"
                                 ;;
         * )                     display_usage
                                 exit 1
     esac
     shift
 done
-
 
 # create data folder if does not exit
 if [ ! -d "$DATA_DIR" ]; then
@@ -42,14 +44,17 @@ if [ ! -d "$KEYSTORE_DIR" ]; then
 fi
 
 # copy keystore file
-cp $NODES_ROOT/UTC* $KEYSTORE_DIR/
+cp $NODES_ROOT/UTC* $KEYSTORE_DIR/ 2>/dev/null
 
 # format command option
-cmd_options="--datadir $DATA_DIR --password /fusion-node/password.txt --mine"
+cmd_options="--datadir $DATA_DIR --password /fusion-node/password.txt"
+
+# cmd_options_local_gtw=' --identity 1 --rpc --ws --rpcaddr 127.0.0.1 --rpccorsdomain 127.0.0.1 --wsapi "eth,net,fsn,fsntx" --rpcapi "eth,net,fsn,fsntx" --wsaddr 127.0.0.1 --wsport 9001 --rpcport 9000'
+cmd_options_local_gtw=' --rpc --ws --rpcaddr 0.0.0.0 --rpccorsdomain 0.0.0.0  --wsapi="eth,net,fsn,fsntx" --rpcapi="eth,net,fsn,fsntx" --wsaddr 0.0.0.0 --wsport 9001 --wsorigins=* --rpcport 9000'
 
 if [ "$ethstats" ]; then
     ethstats=" --ethstats $ethstats:fsnMainnet@node.fusionnetwork.io"
-    cmd_options=$cmd_options$ethstats 
+    cmd_options=$cmd_options$ethstats
 fi
 
 if [ "$unlock" ]; then
@@ -71,19 +76,23 @@ if [ "$unlock" ]; then
     # if [ "$utc_json_address" = "$unlock" ]; then
     #     echo "$unlock is 'valid'"
     # fi
-    
+
     # echo "unlock set to: $unlock"
     unlock=" --unlock $unlock"
-    cmd_options=$cmd_options$unlock 
+    cmd_options=$cmd_options$unlock
 fi
-    
-if [ "$autobt" = true ]; then
+
+if [ "$autobt" = "true" ]; then
     autobt=" --autobt"
-    cmd_options=$cmd_options$autobt 
+    cmd_options=$cmd_options$autobt
 fi
 
-echo "final cmd_options updated to $cmd_options"
+if [ "$mining" = "true" ]; then
+    mining=" --mine"
+    cmd_options=$cmd_options$mining
+fi
 
+echo "efsn flags: $cmd_options$cmd_options_local_gtw"
 
-# efsn  --unlock $unlock --ethstats 
-eval "efsn $cmd_options"
+# efsn  --unlock $unlock --ethstats
+eval "efsn $cmd_options$cmd_options_local_gtw"
