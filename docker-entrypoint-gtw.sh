e5bf3b38ac3578729925932d58bbd347d5a36852
Iruwen <iruwen@gmx.net>
2019-11-27 12:03:49 +0100

    Update docker-entrypoint-gtw.sh to be in line with other scripts
    - changed some formattings
    - fixed argument loop
    - sorted RPC parameters for better readability and added documentation URL

--- docker-entrypoint-gtw.sh
+++ docker-entrypoint-gtw.sh
@@ -5,13 +5,13 @@ DATA_DIR=$NODES_ROOT/data
 ethstats=
 testnet=
 
-display_usage() { 
-    echo "Commands for Fusion local gateway:" 
-    echo -e "\n-e value    Reporting name of a ethstats service" 
-    echo -e "\n-tn         Connect to testnet" 
-    } 
+display_usage() {
+    echo "Commands for Fusion efsn:"
+    echo -e "-e value    Reporting name of a ethstats service"
+    echo -e "-tn         Connect to testnet"
+    }
 
-while [ "$1" != "" ]; do
+while true; do
     case $1 in
         -e | --ethstats )       shift
                                 ethstats=$1
@@ -21,9 +21,8 @@ while [ "$1" != "" ]; do
         * )                     display_usage
                                 exit 1
     esac
-    shift
+    shift || break
 done
-
 
 # create data folder if does not exit
 if [ ! -d "$DATA_DIR" ]; then
@@ -33,8 +32,9 @@ fi
 # format command option
 cmd_options="--datadir $DATA_DIR"
 
-# cmd_options_local_gtw=' --identity 1 --rpc --ws --rpcaddr 127.0.0.1 --rpccorsdomain 127.0.0.1 --wsapi "eth,net,fsn,fsntx" --rpcapi "eth,net,fsn,fsntx" --wsaddr 127.0.0.1 --wsport 9001 --rpcport 9000'
-cmd_options_local_gtw=' --identity 1 --rpc --ws --rpcaddr 0.0.0.0 --rpccorsdomain "*" --wsorigins "*" --wsapi "eth,net,fsn,fsntx" --rpcapi "eth,net,fsn,fsntx" --wsaddr 0.0.0.0 --wsport 9001 --rpcport 9000'
+# Following the geth documentation at https://geth.ethereum.org/docs/rpc/server
+# cmd_options_local_gtw=' --identity 1 --rpc --rpcapi "eth,net,fsn,fsntx" --rpcaddr 127.0.0.1 --rpcport 9000 --rpccorsdomain "*" --ws  --wsapi "eth,net,fsn,fsntx" --wsaddr 127.0.0.1 --wsport 9001 --wsorigins "*"'
+cmd_options_local_gtw=' --identity 1 --rpc --rpcapi "eth,net,fsn,fsntx" --rpcaddr 0.0.0.0 --rpcport 9000 --rpccorsdomain "*" --ws --wsapi "eth,net,fsn,fsntx" --wsaddr 0.0.0.0 --wsport 9001 --wsorigins "*"'
 
 if [ "$testnet" ]; then
     testnet=" --testnet"
@@ -44,14 +44,13 @@ fi
 if [ "$ethstats" ]; then
     if [ "$testnet" ]; then
         ethstats=" --ethstats $ethstats:devFusioInfo2019142@devnodestats.fusionnetwork.io"
-        cmd_options=$cmd_options$ethstats
     else 
         ethstats=" --ethstats $ethstats:fsnMainnet@node.fusionnetwork.io"
-        cmd_options=$cmd_options$ethstats
     fi
+    cmd_options=$cmd_options$ethstats
 fi
 
 echo "flags: $cmd_options$cmd_options_local_gtw"
 
-# efsn  --unlock $unlock --ethstats 
+# efsn  --unlock $unlock --ethstats
 eval "efsn $cmd_options$cmd_options_local_gtw"
