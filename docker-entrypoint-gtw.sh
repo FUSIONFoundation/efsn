9926c24d7bc4af5db66fdb6b5f218ac84fd79184
Iruwen <iruwen@gmx.net>
2020-07-27 12:35:47 +0200

    docker-entrypoint-gtw.sh: replaced eval with exec for proper signal handling

--- docker-entrypoint-gtw.sh
+++ docker-entrypoint-gtw.sh
@@ -53,4 +53,4 @@ fi
 echo "flags: $cmd_options$cmd_options_local_gtw"
 
 # efsn  --unlock $unlock --ethstats
-eval "efsn $cmd_options$cmd_options_local_gtw"
+exec efsn $cmd_options$cmd_options_local_gtw
