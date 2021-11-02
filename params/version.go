d08bf82f23dc38475e9b19e13805129d234a9e66
Philip Fan <fanwengang@hotmail.com>
2021-11-02 17:20:20 +0800

    chore: finalize the mainnet hardfork height

--- params/version.go
+++ params/version.go
@@ -21,11 +21,11 @@ import (
 )
 
 const (
-	VersionMajor = 3 // Major version component of the current release
-	VersionMinor = 7 // Minor version component of the current release
+	VersionMajor = 4 // Major version component of the current release
+	VersionMinor = 0 // Minor version component of the current release
 	VersionPatch = 0 // Patch version component of the current release
 
-	VersionMeta = "unstable" // Version metadata to append to the version string
+	VersionMeta = "stable" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
