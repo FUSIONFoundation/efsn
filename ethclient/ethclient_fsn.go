503a32373eb2e461b2c0a7fb9a0eae84caaee17d
Philip Fan <fanwengang@hotmail.com>
2022-01-17 19:06:11 +0800

    ethclient: add trace internal tx api

--- ethclient/ethclient_fsn.go
+++ ethclient/ethclient_fsn.go
@@ -3,6 +3,8 @@ package ethclient
 import (
 	"context"
 	"github.com/FusionFoundation/efsn/v4/common"
+	"github.com/FusionFoundation/efsn/v4/common/hexutil"
+	"github.com/FusionFoundation/efsn/v4/eth/tracers"
 	"math/big"
 )
 
@@ -28,5 +30,27 @@ func (ec *Client) AssetBalanceAt(ctx context.Context, assetId common.Hash, accou
 func (ec *Client) AssetTimeLockBalanceAt(ctx context.Context, assetId common.Hash, account common.Address, blockNumber *big.Int) (*common.TimeLock, error) {
 	var result common.TimeLock
 	err := ec.c.CallContext(ctx, &result, "fsn_getTimeLockBalance", assetId, account, toBlockNumArg(blockNumber))
+	return &result, err
+}
+
+// CallTrace is the result of a callTracer run.
+type CallTrace struct {
+	Type    string          `json:"type"`
+	From    common.Address  `json:"from"`
+	To      common.Address  `json:"to"`
+	Input   hexutil.Bytes   `json:"input"`
+	Output  hexutil.Bytes   `json:"output"`
+	Gas     *hexutil.Uint64 `json:"gas,omitempty"`
+	GasUsed *hexutil.Uint64 `json:"gasUsed,omitempty"`
+	Value   *hexutil.Big    `json:"value,omitempty"`
+	Error   string          `json:"error,omitempty"`
+	Calls   []CallTrace     `json:"calls,omitempty"`
+}
+
+var callTracer = "callTracer"
+
+func (ec *Client) traceInternalTx(ctx context.Context, tx common.Hash) (*CallTrace, error) {
+	var result CallTrace
+	err := ec.c.CallContext(ctx, &result, "debug_traceTransaction", tx, &tracers.TraceConfig{Tracer: &callTracer})
 	return &result, err
 }
