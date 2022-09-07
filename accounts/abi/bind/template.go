b585382273a380e0556f80934757b453adfba732
Philip Fan <fanwengang@hotmail.com>
2022-09-07 16:12:32 +0800

    fix: abi template

--- accounts/abi/bind/template.go
+++ accounts/abi/bind/template.go
@@ -92,12 +92,12 @@ import (
 	"strings"
 	"errors"
 
-	ethereum "github.com/FusionFoundation/efsn"
-	"github.com/FusionFoundation/efsn/accounts/abi"
-	"github.com/FusionFoundation/efsn/accounts/abi/bind"
-	"github.com/FusionFoundation/efsn/common"
-	"github.com/FusionFoundation/efsn/core/types"
-	"github.com/FusionFoundation/efsn/event"
+	ethereum "github.com/FusionFoundation/efsn/v4"
+	"github.com/FusionFoundation/efsn/v4/accounts/abi"
+	"github.com/FusionFoundation/efsn/v4/accounts/abi/bind"
+	"github.com/FusionFoundation/efsn/v4/common"
+	"github.com/FusionFoundation/efsn/v4/core/types"
+	"github.com/FusionFoundation/efsn/v4/event"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
