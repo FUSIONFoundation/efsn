4a4c8f2e596b811d891fbdb1a7a4b9aaf95f104d
Philip Fan <fanwengang@hotmail.com>
2023-04-04 16:09:07 +0800

    release: 5.0.2

--- params/version.go
+++ params/version.go
@@ -25,7 +25,7 @@ const (
 	VersionMinor = 0 // Minor version component of the current release
 	VersionPatch = 2 // Patch version component of the current release
 
-	VersionMeta = "unstable" // Version metadata to append to the version string
+	VersionMeta = "stable" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
